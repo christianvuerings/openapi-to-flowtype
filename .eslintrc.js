27355d9b4de9c3369c95041d377701e408bbb998
Christian Vuerings <cvuerings@pinterest.com>
2021-01-15 15:45:46 -0800

    Upgrade dependencies + simplify setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 /* eslint-env node */
-const imported = require( '@vlsergey/eslint-config' );
 module.exports = {
-  ...imported,
-  extends: [
-    ...imported.extends,
-    'plugin:flowtype/recommended',
-  ],
-  plugins: [ ...imported.plugins, 'flowtype' ],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
   env: {
     node: true,
     es6: true,
     jest: true,
-  },
-  rules: {
-    ...imported.rules,
-    'import/no-unused-modules': 0,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,109 +1,116 @@
 #! /usr/bin/env node
 // @flow
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// eslint-disable-next-line
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import axios from 'axios';
-import fs from 'fs';
-import Generator from './Generator';
-import path from 'path';
-import prettier from 'prettier';
-import program from 'commander';
-import yaml from 'js-yaml';
+import axios from "axios";
+import fs from "fs";
+import Generator from "./Generator";
+import path from "path";
+import prettier from "prettier";
+import program from "commander";
+import yaml from "js-yaml";
 
 export const DEFAULT_PRETTIER_OPTIONS = {
   parser: 'babel',
 };
 
-export const generator = ( specification : Object, file : string ) => {
-  const generator : Generator = new Generator();
+export const generator = (specification: Object, file: string) => {
+  const generator: Generator = new Generator();
   generator.exact = program.exact || generator.exact;
   generator.lowerCamelCase = program.lowerCamelCase || generator.lowerCamelCase;
   generator.responses = program.responses || generator.responses;
   generator.suffix = program.suffix || generator.suffix;
   generator.verbose = program.verbose || generator.verbose;
 
-  const result : string = generator.generate( specification );
+  const result: string = generator.generate(specification);
 
-  const prettierOptions : any = {
+  const prettierOptions: any = {
     ...DEFAULT_PRETTIER_OPTIONS,
-    ...( prettier.resolveConfig.sync( file ) || {} )
+    ...(prettier.resolveConfig.sync(file) || {}),
   };
-  return prettier.format( result, prettierOptions );
+  return prettier.format(result, prettierOptions);
 };
 
-export const writeToFile = ( dist : string = './flowtype.js', result : string ) => {
-  fs.writeFile( dist, result, err => {
-    if ( err ) {
+export const writeToFile = (dist: string = "./flowtype.js", result: string) => {
+  fs.writeFile(dist, result, (err) => {
+    if (err) {
       throw err;
     }
-  } );
+  });
 };
 
-export const isUrl = ( value : string ) : boolean =>
-  value.match( /https?:\/\// ) !== null;
+export const isUrl = (value: string): boolean =>
+  value.match(/https?:\/\//) !== null;
 
-export const distFile = ( p : Object, inputFileName : string ) : string => {
-  if ( p.destination ) {
+export const distFile = (p: Object, inputFileName: string): string => {
+  if (p.destination) {
     return p.destination;
   }
-  if ( isUrl( inputFileName ) ) {
-    return './flowtype.js';
+  if (isUrl(inputFileName)) {
+    return "./flowtype.js";
   }
 
-  const ext = path.parse( inputFileName ).ext;
-  return inputFileName.replace( ext, '.js' );
+  const ext = path.parse(inputFileName).ext;
+  return inputFileName.replace(ext, ".js");
 };
 
-export const getContentFromFile = ( file : string ) : Object => {
-  const ext = path.extname( file );
-  const readFile = fs.readFileSync( file, 'utf8' );
-  return ext === '.yaml' || ext === '.yml'
-    ? yaml.safeLoad( readFile )
-    : JSON.parse( readFile );
+export const getContentFromFile = (file: string): Object => {
+  const ext = path.extname(file);
+  const readFile = fs.readFileSync(file, "utf8");
+  return ext === ".yaml" || ext === ".yml"
+    ? yaml.safeLoad(readFile)
+    : JSON.parse(readFile);
 };
 
-export const isObject = ( value : any ) : boolean =>
-  typeof value === 'object' && value !== null;
+export const isObject = (value: any): boolean =>
+  typeof value === "object" && value !== null;
 
-export const getContentFromUrl = ( url : string ) : Promise< Object > =>
-  axios( {
-    method: 'get',
+export const getContentFromUrl = (url: string): Promise<Object> =>
+  axios({
+    method: "get",
     url,
-  } ).then( response => {
+  }).then((response) => {
     const { data } = response;
-    return isObject( data ) ? data : yaml.safeLoad( data );
-  } );
+    return isObject(data) ? data : yaml.safeLoad(data);
+  });
 
-export const getContent = ( fileOrUrl : string ) : Promise< Object > => {
-  if ( isUrl( fileOrUrl ) ) {
-    return getContentFromUrl( fileOrUrl );
+export const getContent = (fileOrUrl: string): Promise<Object> => {
+  if (isUrl(fileOrUrl)) {
+    return getContentFromUrl(fileOrUrl);
   }
-  const content = getContentFromFile( fileOrUrl );
-  return Promise.resolve( content );
+  const content = getContentFromFile(fileOrUrl);
+  return Promise.resolve(content);
 };
 
 program
-  .arguments( '<file>' )
-  .option( '-d --destination <destination>', 'Destination path' )
-  .option( '-e --exact', 'Add exact types' )
-  .option( '-v --verbose', 'Verbose output' )
-  .option( '--responses', 'Also generate types for operation responses (with titles)' )
-  .option( '--suffix <suffix>', 'Add suffix (like "Type") to all generated types' )
+  .arguments("<file>")
+  .option("-d --destination <destination>", "Destination path")
+  .option("-e --exact", "Add exact types")
+  .option("-v --verbose", "Verbose output")
   .option(
-    '-l --lower-camel-case',
-    'Transform property keys to lower camel case'
+    "--responses",
+    "Also generate types for operation responses (with titles)"
   )
-  .action( async file => {
+  .option(
+    "--suffix <suffix>",
+    'Add suffix (like "Type") to all generated types'
+  )
+  .option(
+    "-l --lower-camel-case",
+    "Transform property keys to lower camel case"
+  )
+  .action(async (file) => {
     try {
-      const content = await getContent( file );
-      const result = generator( content, file );
-      const dist = distFile( program, file );
-      writeToFile( dist, result );
-      console.log( `Generated flow types to ${dist}` );
-    } catch ( e ) {
-      console.log( e );
+      const content = await getContent(file);
+      const result = generator(content, file);
+      const dist = distFile(program, file);
+      writeToFile(dist, result);
+      console.log(`Generated flow types to ${dist}`);
+    } catch (e) {
+      console.log(e);
     }
-  } )
-  .parse( process.argv );
+  })
+  .parse(process.argv);
